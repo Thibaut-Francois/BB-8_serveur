08e02f387cceaabc1afb623c3d156b106ca3a948
FRANCOIS Thibaut <thibaut.francois@devinci.fr>
2025-03-21 14:46:54 +0100

    add all files

---
+++ index.js
@@ -0,0 +1,41 @@
+const WebSocket = require('ws');
+const ip = require('ip');
+ 
+const PORT = process.env.PORT || 8080; // Render assigne automatiquement un port
+const server = new WebSocket.Server({ port: PORT });
+const ipadress = ip.address();
+ 
+console.log(`Server started on ws://${ipadress}:${PORT}`);
+ 
+let listusers = [];
+ 
+server.on('connection', (ws) => {
+    console.log('Client connecté.');
+    //add user to list
+    listusers.push(ws);
+ 
+    ws.on('message', (message) => {
+        console.log(`Message reçu : ${message}`);
+        //ws.send(`Message bien reçu : ${message}`);
+        //send message to all users
+        let i = 0;
+        listusers.forEach(user => {
+            i++
+            console.log(i)
+            let msg = message.toString();
+            user.send(msg);
+        });
+    });
+ 
+    ws.on('close', () => {
+        console.log('Client déconnecté.');
+        //remove user from list
+        listusers = listusers.filter(user => user !== ws)
+    });
+ 
+    ws.on('error', (err) => {
+        console.error('Erreur WebSocket:', err);
+    });
+});
+ 
+ 
